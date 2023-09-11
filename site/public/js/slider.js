796fbaab3791e502818e04691c6bbd889de05ff6
CreateMonpe <pedro.gomes@sptech.school>
2023-09-11 15:21:41 -0300

    Carrossel automatico, correção rodapé

--- site/public/js/slider.js
+++ site/public/js/slider.js
@@ -1,32 +1,15 @@
 let count = 1;
-document.getElementById("radio1").cheked = true;
+  document.getElementById("radio1").checked = true;
 
-setInterval(function() {
+  setInterval(function() {
     nextImage();
-}, 2000)
+  }, 6000); 
 
-function nextImage() {
+  function nextImage() {
     count++;
-    if (count >5) {
-        count = 1;
+    if (count > 5) {
+      count = 1;
     }
 
-    document.getElementById("radio" + count).cheked = true;
-
-}
-
-const imgs =  document.getElementById("img");
-const img = document.querySelectorAll("#img img")
-
-let idx = 0;
-
-function carrosel() {
-    idx++;
-    if (idx > img.length - 1) {
-        idx = 0;
-    }
-
-    imgs.style.transform = `translateX(${-idx*500} px)`
-}
-
-setInterval(carrosel,1800);
+    document.getElementById("radio" + count).checked = true;
+  }
