2cf5818e0d042337e720fd491315d09cef960702
DaviRdaSilva <davi.rsilva@sptech.school>
2023-09-23 18:48:08 -0300

    Iniciação da atualização do bancp de dados

--- Banco de dados/Script/PowerTechSolutions.sql
+++ Banco de dados/Script/PowerTechSolutions.sql
@@ -1,70 +1,82 @@
-create database PowerTechSolutions;
-use PowerTechSolutions;	
-	
-create table Empresa(
-	idEmpresa int primary key auto_increment,
-    cnpj char(14),
-    email varchar(45),
-    senha varchar(40),
-    filial int,
-    constraint filial foreign key (filial)
-		references Empresa(idEmpresa)
+
+CREATE DATABASE IF NOT EXISTS PowerTechSolutions;
+
+USE PowerTechSolutions;
+
+CREATE TABLE IF NOT EXISTS Empresa(
+	IDEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+    Nome_fantasia VARCHAR(100),
+    Razao_social VARCHAR(100)
 );
 
-create table Usuario(
-	idUsuario int primary key auto_increment,
-    email varchar (45),
-    senha varchar(30),
-    usuarioAdm int,
-    fkEmpresa int,
-    
-    constraint usuarioAdm foreign key (usuarioAdm)
-		references Usuario(idUsuario),
-        
-	constraint fkEmpresa foreign key(fkEmpresa)
-		references Empresa(idEmpresa)
+CREATE TABLE IF NOT EXISTS Filiais(
+	IDFilial INT PRIMARY KEY AUTO_INCREMENT,
+    Cnpj CHAR(18),
+    Apelido_interno VARCHAR(100),
+    FKEmpresa INT,
+		CONSTRAINT FKEmpresa_Filial FOREIGN KEY (FKEmpresa)
+			REFERENCES Empresa(idEmpresa)
 );
 
-create table HelpDesk(
-	idHd int primary key auto_increment,
-    problema varchar(200),
-    horario datetime,
-    fkUsuario int,
-    
-    constraint fkUsuario foreign key (fkUsuario)
-		references Usuario(idUsuario)
+CREATE TABLE IF NOT EXISTS Endereco_empresa(
+	IDEndereco_empresa INT PRIMARY KEY AUTO_INCREMENT,
+    Cep CHAR(8),
+    Rua VARCHAR(100),
+    Numero INT,
+    Bairro VARCHAR(100),
+    Cidade VARCHAR(100),
+    Estado VARCHAR(100),
+    FKFilial INT,
+		CONSTRAINT FKEndereco_Filial FOREIGN KEY (FKFilial)
+			REFERENCES Filiais(IDFilial)
 );
 
-create table Maquina(
-	idMaquina int primary key auto_increment,
-    tipo varchar(30), 
-    
-    so varchar(45),
-    constraint chk check (so in('Windows','Linux')));
-    
-create table Monitoramento(
-	idMonitoramento int auto_increment,
-    fkUser int,
-    fkMaquina int,
-    CPU int,
-    ram decimal(2),
-    disco int,
-    dt datetime,
-    
-    constraint pkCompsota primary key(idMonitoramento, fkUser, fkMaquina),
-    
-    constraint chkM check (usb in(0,1)),
-    
-    constraint fkUser foreign key(fkUser)
-		references Usuario(idUsuario),
-	
-    constraint fkMaquina foreign key(fkMaquina)
-		references Maquina(idMaquina)
+CREATE TABLE IF NOT EXISTS Setor(
+	IDSetor INT PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(80)
 );
 
-create table Localidade(
-	idLocal int primary key auto_increment,
-    andar int,
-    sala int,
-    setor varchar(30)
+CREATE TABLE IF NOT EXISTS Andar(
+	IDAndar INT PRIMARY KEY AUTO_INCREMENT,
+    Nomeacao VARCHAR(50),
+    FKEndereco INT,
+    FKSetor INT,
+		CONSTRAINT FKAndar_ENdereco FOREIGN KEY (FKEndereco)
+			REFERENCES Endereco_empresa(IDEndereco_empresa),
+		CONSTRAINT FKAndar_Setor FOREIGN KEY (FKSetor)
+			REFERENCES Setor(IDSetor)
+);
+
+CREATE TABLE IF NOT EXISTS Sala(
+	IDSala INT PRIMARY KEY AUTO_INCREMENT,
+    apelido VARCHAR(50),
+    FKAndar INT,
+		CONSTRAINT FKSala_Andar FOREIGN KEY (FKAndar)
+			REFERENCES Andar(IDAndar)
+);
+
+CREATE TABLE IF NOT EXISTS Local_trabalho(
+	IDLocal_trabalho INT PRIMARY KEY AUTO_INCREMENT,
+    FKSala INT,
+    FKFilial INT,
+		CONSTRAINT FKLocal_Sala FOREIGN KEY (FKSala)
+			REFERENCES Sala(IDSala),
+		CONSTRAINT FKLocal_Filial FOREIGN KEY (FKFilial)
+			REFERENCES Filiais(IDFilial)
+);
+
+CREATE TABLE IF NOT EXISTS Funcionario(
+	IDFuncionairo INT PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(150),
+    Cpf CHAR(11),
+    Email VARCHAR(100),
+    FKFilial INT,
+		CONSTRAINT FKFuncionario_Filial FOREIGN KEY (FKFilial)
+			REFERENCES Filiais(IDFilial)
+);
+
+CREATE TABLE IF NOT EXISTS Maquinhas_fisicas(
+	IDMaquina INT PRIMARY KEY AUTO_INCREMENT,
+	Marca VARCHAR(80),
+    Total_Ram 
 );
