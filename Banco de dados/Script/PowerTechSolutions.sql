69995ead7c510d3fb05422c9825bb5bb92bd2890
DaviRdaSilva <davi.rsilva@sptech.school>
2023-09-27 09:58:35 -0300

    Atualização da MOdelagem de dados (aguardando revisão e aprovação da professora)

--- Banco de dados/Script/PowerTechSolutions.sql
+++ Banco de dados/Script/PowerTechSolutions.sql
@@ -56,13 +56,14 @@ CREATE TABLE IF NOT EXISTS Sala(
 );
 
 CREATE TABLE IF NOT EXISTS Local_trabalho(
-	IDLocal_trabalho INT PRIMARY KEY AUTO_INCREMENT,
+	IDLocal_trabalho INT AUTO_INCREMENT,
     FKSala INT,
     FKFilial INT,
 		CONSTRAINT FKLocal_Sala FOREIGN KEY (FKSala)
 			REFERENCES Sala(IDSala),
 		CONSTRAINT FKLocal_Filial FOREIGN KEY (FKFilial)
-			REFERENCES Filiais(IDFilial)
+			REFERENCES Filiais(IDFilial),
+		CONSTRAINT PKSala_Filial PRIMARY KEY (IDLocal_trabalho,FKSala,FKFilial)
 );
 
 CREATE TABLE IF NOT EXISTS Funcionario(
@@ -78,5 +79,144 @@ CREATE TABLE IF NOT EXISTS Funcionario(
 CREATE TABLE IF NOT EXISTS Maquinhas_fisicas(
 	IDMaquina INT PRIMARY KEY AUTO_INCREMENT,
 	Marca VARCHAR(80),
-    Total_Ram 
+    Total_Ram FLOAT,
+    Total_Disco FLOAT,
+    Modelo_processador VARCHAR(50),
+    Total_entradas_USB INT,
+    Maxima_tranferencia_rede_upload FLOAT,
+	Maxima_tranferencia_rede_download FLOAT,
+    FKFuncionario INT,
+		CONSTRAINT FKMaquina_funcionario FOREIGN KEY (FKFuncionario)
+			REFERENCES Funcionario(IDFuncionario)
 );
+
+CREATE TABLE IF NOT EXISTS Servidores(
+	IDServidor INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(80),
+    FKFilial INT,
+		CONSTRAINT FKServidor_Filial FOREIGN KEY (FKFilial)
+			REFERENCES Filiais(IDFilial)
+);
+
+CREATE TABLE IF NOT EXISTS Endereco_Hidreletrica(
+	idEndereco INT PRIMARY KEY AUTO_INCREMENT,
+    Rio VARCHAR(80),
+    Bacia_hidrografica VARCHAR(80),
+	Sub_bacia_hidrografica VARCHAR(80),
+    UF CHAR(2)
+);
+
+CREATE TABLE IF NOT EXISTS Hidreletrica(
+	idHidreletrica INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    FKEndereco INT,
+		CONSTRAINT FKHidreletrica_endereco FOREIGN KEY (FKEndereco)
+			REFERENCES Endereco_Hidreletrica(idEndereco)
+);
+
+CREATE TABLE IF NOT EXISTS Maquinhas_virtuais(
+	IDMaquina INT PRIMARY KEY AUTO_INCREMENT,
+    Total_Ram FLOAT,
+    Total_Disco FLOAT,
+    Modelo_processador VARCHAR(50),
+    Total_entradas_USB INT,
+    Maxima_tranferencia_rede_upload FLOAT,
+	Maxima_tranferencia_rede_download FLOAT,
+    FKHidreletrica INT,
+		CONSTRAINT FKMaquina_hidreletrica FOREIGN KEY (FKHidreletrica)
+			REFERENCES Hidreletrica(idHidreletrica)
+);
+
+CREATE TABLE IF NOT EXISTS Componentes_cadastrados(
+	IDComponentes INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(80)
+);
+
+CREATE TABLE IF NOT EXISTS Componentes_Monitorados_mf(
+	IDComponentes INT AUTO_INCREMENT,
+    FKComponente_cadastrado INT,
+    FKMaquina_Fisica INT,
+		CONSTRAINT FKComponente_C_Componentes_M FOREIGN KEY (FKComponente_cadastrado)
+			REFERENCES Componentes_cadastrados(IDComponentes),
+		CONSTRAINT FKMaquinaFisica_Componentes_M FOREIGN KEY (FKMaquina_Fisica)
+			REFERENCES Maquinhas_fisicas(IDMaquina),
+		CONSTRAINT PKComponentes_M_Componentes_C PRIMARY KEY (IDComponentes,FKComponente_cadastrado)
+);
+
+CREATE TABLE IF NOT EXISTS Componentes_Monitorados_mv(
+	IDComponentes INT AUTO_INCREMENT,
+    FKComponente_cadastrado INT,
+    FKMaquina_virtual INT,
+		CONSTRAINT FKComponente_C_Componentes_M FOREIGN KEY (FKComponente_cadastrado)
+			REFERENCES Componentes_cadastrados(IDComponentes),
+		CONSTRAINT FKMaquinaVirtual_Componentes_M FOREIGN KEY (FKMaquina_virtual)
+			REFERENCES Maquinhas_virtuais(IDMaquina),
+		CONSTRAINT PKComponentes_M_Componentes_C PRIMARY KEY (IDComponentes,FKComponente_cadastrado)
+);
+
+CREATE TABLE IF NOT EXISTS Monitoramento_raw_mf(
+	IDMonitoramento INT PRIMARY KEY AUTO_INCREMENT,
+    Data DATE DEFAULT CURRENT_TIMESTAMP,
+    Hora TIME DEFAULT CURRENT_TIMESTAMP,
+    dado_capturado FLOAT,
+    FKComponente_monitorado INT,
+		CONSTRAINT FKMonitoramento_componente FOREIGN KEY (FKCompoenente_monitorado)
+			REFERENCES Componentes_Monitorados_mf(IDComponentes)
+);
+
+CREATE TABLE IF NOT EXISTS Monitoramento_raw_mv(
+	IDMonitoramento INT PRIMARY KEY AUTO_INCREMENT,
+    Data DATE DEFAULT CURRENT_TIMESTAMP,
+    Hora TIME DEFAULT CURRENT_TIMESTAMP,
+    dado_capturado FLOAT,
+    FKComponente_monitorado INT,
+		CONSTRAINT FKMonitoramento_componente FOREIGN KEY (FKCompoenente_monitorado)
+			REFERENCES Componentes_Monitorados_mv(IDComponentes)
+);
+
+CREATE TABLE IF NOT EXISTS Monitoramento_trusted_mf(
+	IDMonitoramento INT PRIMARY KEY AUTO_INCREMENT,
+    Data DATE DEFAULT CURRENT_TIMESTAMP,
+    Hora TIME DEFAULT CURRENT_TIMESTAMP,
+    dado_capturado FLOAT,
+    FKComponente_monitorado INT
+);
+
+CREATE TABLE IF NOT EXISTS Monitoramento_trusted_mv(
+	IDMonitoramento INT PRIMARY KEY AUTO_INCREMENT,
+    Data DATE DEFAULT CURRENT_TIMESTAMP,
+    Hora TIME DEFAULT CURRENT_TIMESTAMP,
+    dado_capturado FLOAT,
+    FKComponente_monitorado INT
+);
+
+CREATE TABLE IF NOT EXISTS Permissoes(
+	IDPermissao INT PRIMARY KEY AUTO_INCREMENT,
+    Visualizar BOOLEAN,
+    Editar BOOLEAN,
+    Cadastrar BOOLEAN,
+    Deletar BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS Nivel_acesso(
+	IDNivel_acesso INT AUTO_INCREMENT,
+    nome VARCHAR(80),
+    FKPermissao INT,
+		CONSTRAINT FKPermissao_Nivel_acesso FOREIGN KEY (FKPermissao)
+			REFERENCES Permissoes(IDPermissao),
+		CONSTRAINT PKNivel_acesso_permissao PRIMARY KEY (IDNivel_acesso,FKPermissao)
+);
+
+CREATE TABLE IF NOT EXISTS Usuario_Dashboard(
+	IDUsuario INT AUTO_INCREMENT,
+    nome VARCHAR(100),
+    email VARCHAR(100),
+    cpf CHAR(11),
+    senha CHAR(8),
+    FKFilial INT,
+		CONSTRAINT FKFilial_funcionario 
+    FKNivel_acesso INT,
+    
+);
+
+
