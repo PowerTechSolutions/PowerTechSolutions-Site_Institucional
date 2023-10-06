c6c31ecf04bf2e08f39e67e4115bf49461564119
DaviRdaSilva <davi.rsilva@spetch.school>
2023-10-06 11:32:10 -0300

    Começo da arrumação com o node

--- site/public/js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Adicione um evento de clique ao botão
-    document.getElementById("scrollButton").addEventListener("click", function() {
-        // Use scrollIntoView() para rolar para a seção desejada
-        document.getElementById("outraSessao").scrollIntoView({ behavior: "smooth", block: "start" });
-    });
-});
