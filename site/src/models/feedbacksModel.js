e8798d1d6cb4a18daa1bbce89e828c8b85de6bf7
DaviRdaSilva <davi.rsilva@spetch.school>
2023-10-07 19:17:08 -0300

    Inicio da segunda pagina do dashboard(viewPoinnt)

--- site/src/models/feedbacksModel.js
+++ site/src/models/feedbacksModel.js
@@ -15,7 +15,7 @@ function cadastrarFeedbacks(feedback, estrela) {
 function listar_feedbacks() {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar_usuario()");
     var instrucao = `
-    SELECT * FROM Feedbacks ORDER BY IDFeedback ASC LIMIT 3;
+    SELECT * FROM Feedbacks ORDER BY IDFeedback DESC LIMIT 3;
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
