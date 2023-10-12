8853f006893abf1918c36388c583560f5422ffe5
kaori_katayama <michelly.katayama@sptech.school>
2023-10-12 15:42:37 -0300

    DASHBOARD: DONE -> CHAT BOX: IN PROGRESS

---
+++ site/public/js/chat.js
@@ -0,0 +1,17 @@
+var show_lit = false
+function literate() {
+    if (show_lit == true) {
+        show_lit = false
+        lit_dropdown.style.display = 'none'
+    } else {
+        show_lit = true
+        lit_dropdown.style.display = 'block'
+    }
+}
+
+function clickclose3() {
+    if (show_lit == true) {
+        show_lit = false
+        lit_dropdown.style.display = 'none'
+}
+}
